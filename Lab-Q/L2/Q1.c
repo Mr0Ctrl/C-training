95f40e2e08ae9e5e5b00b77c4d55e2b29de1a692
mr0ctrl <a.atkn.dmr@gmail.com>
2024-02-12 19:21:46 +0300

    test

---
+++ Lab-Q/L2/Q1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    int a;
+    int b;
+    int c;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+
+    int n;
+
+    if (a < c)
+    {
+        n = a;
+        a = c;
+        c = n;
+    }
+
+    if (a < b)
+    {
+        n = a;
+        a = b;
+        b = n;
+    }
+    
+    if (c > b)
+    {
+        n = c;
+        c = b;
+        b = n;
+    }
+
+    printf("%d %d %d",a,b,c);
+    
+    return 0;
+}
